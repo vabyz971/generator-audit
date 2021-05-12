3804181f65cc7f5e19222e1f049d2ac08e90e069
vabyz971 <vabyz971@gmail.com>
2021-05-12 11:50:19 -0400

    update form inscription

--- core/audit/forms.py
+++ core/audit/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.forms import widgets
 from .models import Audit, Exploit
 
 
@@ -8,6 +9,12 @@ class AddNewUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+        widgets = {
+            'username' : widgets.TextInput(attrs={'class' : 'form-control'}),
+            'email' : widgets.EmailInput(attrs={'class' : 'form-control'}),
+            'password1' : widgets.PasswordInput(attrs={'class' : 'form-control'}),
+            'password2' : widgets.PasswordInput(attrs={'class' : 'form-control'},),
+        }
 
 
 class AuditForm(forms.ModelForm):
